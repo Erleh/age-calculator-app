27bb8b69e2e20b69ee0f216d2debb008bc611a2e
Eric Lee <eric.lee0051@gmail.com>
2023-09-24 02:10:29 -0700

    Initial commit, Initialized React App using Vite, Added components to represent basic elements within the design

---
+++ Age-Calculator/src/App.jsx
@@ -0,0 +1,106 @@
+import './App.css'
+
+// Breaks down the provided design into React Components
+// Individual inputs within a submission type form, and the results from the calculation
+
+// To-do :
+//    styling
+//    age calculation
+//    verify inputs for day, month, and year input
+//    responsiveness to:
+//      invalid input
+//      submission
+//      hover state of:
+//        input
+//        button
+//    Change style for larger screens --> currently mobile first
+
+function Input({display, inputName}) {
+  return (
+    <li>
+      <label htmlFor={inputName}>{display}</label>
+      <input id={inputName} type="number"></input>
+    </li>
+  )
+}
+
+// To-do:
+//  Input validation
+//  Submit data into calculation
+//  Responsiveness
+function DateInputs() {
+  return (
+    <ul>
+      <Input 
+        display={"day"}
+        inputName={"day"}
+      />
+      <Input 
+        display={"month"} 
+        inputName={"month"}
+      />
+      <Input 
+        display={"year"} 
+        inputName={"year"}
+      />
+    </ul>
+  )
+}
+
+function DateForm() {
+  return (
+    <form>
+      <DateInputs />
+      <input type="submit"></input>
+    </form>
+  )
+}
+
+// To-do:
+//  Styling
+//  Calculation
+function Result({display, idName}) {
+  return (
+    <li id={idName}>
+      <span>--</span> {display}
+    </li>
+  )
+}
+
+function Results() {
+  return (
+    <ul>
+      <Result 
+        display={"years"}
+        idName={"years-res"} 
+      />
+      <Result 
+        display={"months"}
+        idName={"months-res"}
+      />
+      <Result 
+        display={"days"} 
+        idName={"days-res"}
+      />
+    </ul>
+  )
+}
+
+// To-do:
+// Should style card here
+function Calculator() {
+  return (
+    <div>
+      <DateForm />
+      <Results />
+    </div>
+  )
+}
+
+function App() {
+  return (
+    <Calculator />
+  )
+}
+
+export default App
